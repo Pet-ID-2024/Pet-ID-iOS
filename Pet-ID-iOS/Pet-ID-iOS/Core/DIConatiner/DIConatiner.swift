bd5703b02bef4309a7731ca67859b2350f738d47
HyeonJun Kang <khjj9865@naver.com>
2024-06-30 22:25:51 +0900

    add: DIContainer

--- Pet-ID-iOS/Pet-ID-iOS/Core/DIConatiner/DIConatiner.swift
+++ Pet-ID-iOS/Pet-ID-iOS/Core/DIConatiner/DIConatiner.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
+//  DIConatiner.swift
 //  Pet-ID-iOS
 //
 //  Created by 강현준 on 6/30/24.
 //
 
 import Foundation
+import Swinject
+
+final class DIContainer {
+    
+    static let shared = DIContainer()
+    
+    private let container: Container
+    
+    private init() {
+        self.container = Container()
+    }
+    
+    func resolve<T>(_ serviceType: T.Type) -> T {
+        return container.resolve(serviceType)!
+    }
+    
+    func register<T>(interface: T.Type, implement: @escaping ((Resolver) -> T)) {
+        container.register(interface, factory: implement)
+    }
+}
